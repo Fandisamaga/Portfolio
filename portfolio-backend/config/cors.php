2c1d8273be23f8a93224ec480d4fe665ccd09d61
Fandisamaga <fandisamaga@email.com>
2025-07-11 12:07:48 +0800

    Initial commit: Allow all Vercel subdomains in CORS

--- portfolio-backend/config/cors.php
+++ portfolio-backend/config/cors.php
@@ -4,9 +4,11 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-    'http://localhost:3000',
-    'https://portfolio-rouge-two-77.vercel.app',],
-    'allowed_origins_patterns' => [],
+        'http://localhost:3000',
+    ],
+    'allowed_origins_patterns' => [
+        '~.vercel\\.app$~'
+    ],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
